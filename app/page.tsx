75feb9d564e308c50855794fe5f755fdfd54eb22
Houssem Amiri <houssem.amiri@proton.me>
2024-09-10 21:36:31 -0400

    feat: remove css and configure radix ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import styles from "./page.module.css";
-
+import { Flex, Text, Button } from "@radix-ui/themes";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Personal budget tracker</h1>
-    </main>
+    <Flex
+      direction="column"
+      gap="2"
+      justify="center"
+      align="center"
+      content="center"
+      height="100vh"
+    >
+      <Text>Personal budget app</Text>
+      <Button>Let&apos;s go</Button>
+    </Flex>
   );
 }
